076ae45b4a92af8746da4f0fc2476f773f176a07
Ilya Blinkov <ilyarbox4@gmail.com>
2018-08-30 11:05:08 +0500

    Add RenderableObject class

--- main.js
+++ main.js
@@ -1,3 +1,12 @@
+class Vector {
+  constructor(x, y) {
+    this.x = x ? x : 0;
+    this.y = y ? y : 0;
+  }
+
+  reverseX() { this.x *= -1; }
+}
+
 class World {
   constructor() {
     this.scene = {
@@ -10,22 +19,54 @@ class World {
     this._tick();
   }
 
-  _tick() {
-    this._update();
-    this.render();
-    requestAnimationFrame(this._tick.bind(this));
-  }
-
-  _update() {
+  update() {
     var objects = this.scene.background.concat(this.scene.front);
-    console.log(objects);
+    if (!objects[0]) throw new Error('No objects to render');
+    objects.forEach(obj => obj.update());
   }
 
   render() {
     this.scene.background.forEach(obj => obj.render());
     this.scene.front.forEach(obj => obj.render());
   }
+
+  addBackground(constructor, config) {
+    this._addAny('background', constructor, config);
+  }
+
+  addFront(constructor, config) {
+    this._addAny('front', constructor, config);
+  }
+
+  _addAny(store, constructor, config) {
+    if (!config) config = {};
+    config.world = this;
+    var obj = new constructor(config);
+    this.scene[store].push(obj);
+  }
+
+  _tick() {
+    this.update();
+    this.render();
+    requestAnimationFrame(this._tick.bind(this));
+  }
+}
+
+class _Object {
+  constructor(config) {
+    this.world = config.world;
+    this.loc = new Vector();
+  }
+
+  update() {
+    
+  }
+
+  render() {
+
+  }
 }
 
 var world = new World();
+world.addFront(_Object);
 world.start();
